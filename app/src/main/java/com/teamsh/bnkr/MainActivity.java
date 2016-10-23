fdc6c08499f7880ea22af188f394feaa3a4c3fd9
klee166 <klee166@jhu.edu>
2016-10-23 00:34:11 -0400

    disabled a back button at MainActivity

--- app/src/main/java/com/teamsh/bnkr/MainActivity.java
+++ app/src/main/java/com/teamsh/bnkr/MainActivity.java
@@ -120,4 +120,8 @@ public class MainActivity extends AppCompatActivity {
 
         PebbleKit.sendDataToPebble(getApplicationContext(), appUuid, dict);
     }
+
+    public void onBackPressed() {
+        //void
+    }
 }
